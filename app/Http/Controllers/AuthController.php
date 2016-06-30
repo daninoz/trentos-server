ec4fa3f348b924f2cd000c387b7ab70914503c94
Daniel Albornoz <daniel@daninoz.com>
2016-06-29 22:03:50 -0300

    FIx auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -89,7 +89,7 @@ class AuthController extends Controller {
             $user->facebook = $profile['id'];
             $user->email = $profile['email'];
             $user->displayName = $profile['name'];
-            $user->is_adim = 1;
+            $user->is_admin = 1;
             $user->save();
 
             return response()->json(['token' => $this->createToken($user)]);
